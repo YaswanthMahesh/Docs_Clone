1ab03a52b97a3ae4b3d9817a92713afd485a4ca7
YFA23SCM92N <yaswanthmahesh007@gmail.com>
2024-08-12 17:24:31 -0500

    Initial commit

---
+++ src/main/java/com/docs/docs_clone/DocsCloneApplication.java
@@ -0,0 +1,13 @@
+package com.docs.docs_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocsCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docs/docs_clone/DocsCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docs.docs_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocsCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
